5448a67c3e4a2a0c0e4d7f7f9d93f5987bbf69ca
Bismarrck <Bismarrck@me.com>
2015-01-20 17:26:25 +0800

    Initial Commit

---
+++ FastAffinityPropagation/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  FastAffinityPropagation
+//
+//  Created by Bismarrck on 1/20/15.
+//  Copyright (c) 2015 Nexd. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
+
